cff19215ef8d4258483607f27e2730ff6fd2a158
Carlos Guerra Blanco <cguerra@MacBook-Pro-de-Carlos.local>
2023-02-27 13:19:46 +0100

    add linkedin link and profile photo

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img className="imgMain" src={require('./assets/img/carlos.jpeg')}/>
         <p>
           <code>Carlos Guerra Blanco, Full Stack Developer</code>
-        </p>
-        <p>
           <code>Write me at cguerradev@gmail.com</code>
         </p>
       </header>
+      <footer>
+        <a target="_blank" href="https://es.linkedin.com/in/carlos-guerra-blanco"><img className="imgLinkedin" src={require('./assets/img/linkedin.png')}/></a>
+      </footer>
     </div>
   );
 }
