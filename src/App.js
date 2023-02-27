b8d00c920368e013ab25d04b68b40edf5dfe1530
Carlos Guerra Blanco <cguerra@MacBook-Pro-de-Carlos.local>
2023-02-27 23:37:51 +0100

    skills section add

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <span className="circleApple yellow"></span>
         <span className="circleApple green"></span>
         <code>
-          <h3>Mis trabajos:</h3>
+          <h3>main@projects:~$</h3>
           <ul>
             <li><a target="_blank" rel="noreferrer" href="https://ticjob.es/">ticjob.es</a></li>
             <li><a target="_blank" rel="noreferrer" href="https://thisisaluche.com/">thisisaluche.com</a></li>
@@ -22,8 +22,22 @@ function App() {
           </ul>
         </code>
       </section>
+      <section className="myTechs">
+        <h3>Skills</h3>
+        <ul>
+          <li className="color-one"><a href="https://es.wikipedia.org/wiki/Java_(lenguaje_de_programaci%C3%B3n)">java</a></li>
+          <li className="color-two"><a href="https://es.wikipedia.org/wiki/HTML5">HTML5</a></li>
+          <li className="color-one"><a href="https://es.wikipedia.org/wiki/JavaScript">javascript</a></li>
+          <li className="color-three"><a href="https://es.wikipedia.org/wiki/Bash">bash</a></li>
+          <li className="color-one"><a href="https://es.wikipedia.org/wiki/JavaServer_Pages">JSP</a></li>
+          <li className="color-two"><a href="https://es.wikipedia.org/wiki/CSS">CSS3</a></li>
+          <li className="color-three"><a href="https://es.wikipedia.org/wiki/GNU/Linux">linux</a></li>
+          <li className="color-two"><a href="https://es.wikipedia.org/wiki/Bootstrap_(framework)">bootstrap</a></li>
+          <li className="color-two"><a href="https://es.wikipedia.org/wiki/JQuery">jquery</a></li>
+        </ul>
+      </section>
       <footer>
-        <p>¡Escríbeme a <a className="bold" href="mailto:cguerradev@gmail.com">cguerradev@gmail.com</a>!</p>
+        <p>¡Escríbeme a <a href="mailto:cguerradev@gmail.com"><strong>cguerradev@gmail.com</strong></a>!</p>
         <a target="_blank" rel="noreferrer" href="https://es.linkedin.com/in/carlos-guerra-blanco"><img alt="logo linkedin" className="imgLinkedin" src={require('./assets/img/linkedin.png')}/></a>
       </footer>
     </div>
